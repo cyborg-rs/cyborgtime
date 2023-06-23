c36aee53c0bfdca7580f01d988803a4f4409d676
pinkforest <36498018+pinkforest@users.noreply.github.com>
2023-06-23 14:52:51 +0000

    The best fmt is the fmt fmt

--- benches/datetime_parse.rs
+++ benches/datetime_parse.rs
@@ -1,44 +1,32 @@
-use chrono::{DateTime};
+use chrono::DateTime;
 use cyborgtime::parse_rfc3339;
 
 #[bench]
 fn rfc3339_cyborgtime_seconds(b: &mut test::Bencher) {
-    b.iter(|| {
-        parse_rfc3339("2018-02-13T23:08:32Z").unwrap()
-    });
+    b.iter(|| parse_rfc3339("2018-02-13T23:08:32Z").unwrap());
 }
 
 #[bench]
 fn datetime_utc_parse_seconds(b: &mut test::Bencher) {
-    b.iter(|| {
-        DateTime::parse_from_rfc3339("2018-02-13T23:08:32Z").unwrap()
-    });
+    b.iter(|| DateTime::parse_from_rfc3339("2018-02-13T23:08:32Z").unwrap());
 }
 
 #[bench]
 fn rfc3339_cyborgtime_millis(b: &mut test::Bencher) {
-    b.iter(|| {
-        parse_rfc3339("2018-02-13T23:08:32.123Z").unwrap()
-    });
+    b.iter(|| parse_rfc3339("2018-02-13T23:08:32.123Z").unwrap());
 }
 
 #[bench]
 fn datetime_utc_parse_millis(b: &mut test::Bencher) {
-    b.iter(|| {
-        DateTime::parse_from_rfc3339("2018-02-13T23:08:32.123Z").unwrap()
-    });
+    b.iter(|| DateTime::parse_from_rfc3339("2018-02-13T23:08:32.123Z").unwrap());
 }
 
 #[bench]
 fn rfc3339_cyborgtime_nanos(b: &mut test::Bencher) {
-    b.iter(|| {
-        parse_rfc3339("2018-02-13T23:08:32.123456983Z").unwrap()
-    });
+    b.iter(|| parse_rfc3339("2018-02-13T23:08:32.123456983Z").unwrap());
 }
 
 #[bench]
 fn datetime_utc_parse_nanos(b: &mut test::Bencher) {
-    b.iter(|| {
-        DateTime::parse_from_rfc3339("2018-02-13T23:08:32.123456983Z").unwrap()
-    });
+    b.iter(|| DateTime::parse_from_rfc3339("2018-02-13T23:08:32.123456983Z").unwrap());
 }
